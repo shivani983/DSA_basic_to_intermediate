d69d0b1087b4a334c3c6285fc9c439c7dbfdbec6
shivani <167305474+shivani983@users.noreply.github.com>
2025-01-12 09:40:48 +0530

    searching an element in array using binary search

--- searching_an_element_in_array_using_binary_search.py
+++ searching_an_element_in_array_using_binary_search.py
@@ -1,20 +1,22 @@
 class Solution:
     def binarysearch(self, arr, k):
         # Code Here
-        n = len(arr)
         low = 0
-        high = n-1
+        high = len(arr)-1
         target = k
+        result = -1
         while low <= high :
             mid = (low+high)//2
+            
             if arr[mid] == target:
-                return mid
+                result =  mid
+                high = mid-1
             
             elif arr[mid] > target:
                 high = mid-1
                 
-            elif arr[mid] < target:
-                low = mid+1
-            
             else:
-                return -1
+                low = mid+1
+        
+                   
+        return result
