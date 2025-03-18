716dea01869e96e298208f79cc536080e12da293
shivani <167305474+shivani983@users.noreply.github.com>
2025-03-18 10:02:34 +0530

    print function

---
+++ print_func.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+# Function to print x in decreasing order
+def printInDecreasing(x):
+    # Complete the code below
+    while(x >= 0):
+        
+        # your statement below to print the number
+        # in decreasing order
+        # Note: use end=" " parameter with print to seperate numbers by space.
+        ##Output for testcases will automatically separated by a new line by the print() in driver code
+        print(x ,end = " ")
+        
+        
+        x -= 1
