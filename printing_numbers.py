6d42a68cc32e4109eab71fb3783260cfd24d9f55
shivani <167305474+shivani983@users.noreply.github.com>
2025-03-18 12:08:12 +0530

    printing numbers

---
+++ printing_numbers.py
@@ -0,0 +1,14 @@
+
+def pos(n):
+    ## Write the code
+    while (n > 0):
+        print(n-1,end = " ")
+        n-=1
+        
+    
+def neg(n):
+    ##Write the code
+    while n <=0:
+        print(n,end=" ")
+        n+=1
+    
